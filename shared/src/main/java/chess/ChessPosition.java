6e7dca15f1bfb6a056e73a5dd19b51c7b23d69e3
Hamid Jaeyoung Jahangir <hamidjjahangir@gmail.com>
2026-09-12 12:23:53 -0600

    Overrode the equals() and hashCode() methods using IntelliJ's built in overrider.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -14,6 +16,21 @@ public class ChessPosition {
         this.row = row;
         this.col = col;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
+
     /**
      * @return which row this position is in
      * 1 codes for the bottom row
